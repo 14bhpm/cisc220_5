66612fdb61b42a92252887ad44e80369b01f5bec
Beltan Michel <14bhpm@queensu.ca>
2016-12-01 22:51:56 -0500

    Added names and student IDs

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
+//Rawan Alsaadi 10081442
+//Omar Toutounji 10169506
+//Beltan Michel 10169162
+
 #include "udll.h"
+
 /**
  * Inserts a new node at the beginning of the doubly linked list
  *
--- udll.c
+++ udll.c
@@ -1,3 +1,7 @@
+//Rawan Alsaadi 10081442
+//Omar Toutounji 10169506
+//Beltan Michel 10169162
+
 #include "udll.h"
 #include <stdlib.h>
 
